b7f54b081f675329145dde9d8d1124b5430016e7
SuhailAbdi <tqw257@alumni.ku.dk>
2021-11-10 16:57:47 +0100

    finalized

---
+++ src/mandatory3/feigenbaumClass.java
@@ -0,0 +1,34 @@
+package mandatory3;
+
+public class feigenbaumClass {
+
+    private double lambda = 1;
+    private double out = 0.1;
+
+    public void setLambda(double lambda) {
+        this.lambda = lambda;
+    }
+
+    public void setOut(double out) {
+        this.out = out;
+    }
+
+    public double[] feigenbaum() {
+
+        out = lambda * out * (1 - out);
+
+        return new double[] {out, lambda};
+    }
+
+    public void increaseLambda() {
+
+        if (lambda < 4) {
+            lambda += 0.1;
+
+            for (int i = 0; i < 500; i++) {
+                out = lambda * out * (1 - out);
+            }
+        }
+
+    }
+}
