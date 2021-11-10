406ada9ebf0fe2adef59577bea59b5f216ff9b6e
SuhailAbdi <tqw257@alumni.ku.dk>
2021-11-10 15:26:51 +0100

    testing func needed

--- src/mandatory3/Main.java
+++ src/mandatory3/Main.java
@@ -1,8 +1,28 @@
 package mandatory3;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        //testing threads
 	    System.out.println("elloello");
+
+	    Thread thread = new Thread(){
+	        @Override
+            public void run(){
+                System.out.println("test");
+            }
+        };
+	    thread.run();
+
+	    //testing making a window
+        JFrame frame = new JFrame("frame test");
+        Label emptyLabel = new Label("sup");
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
